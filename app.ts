d1f33a00cd20bf9b4f12356a0c97581525150fea
Djamalayl <djamalayl32@mail.ru>
2023-06-05 13:16:00 +0300

    first commit

---
+++ app.ts
@@ -0,0 +1,16 @@
+const books:IBook[] = [
+    { title: '1984', author: 'George Orwell', year: 1949 },
+    { title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+];
+
+function getBooksByAuthor(author:string):IBook[] {
+    return books.filter(book => book.author === author);
+}
+
+console.log(getBooksByAuthor('George Orwell'));
+
+interface IBook {
+    title:string,
+    author:string,
+    year:number,
+}
